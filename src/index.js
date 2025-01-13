8c04475df6c49dc1911d57af782cf39df5b4b9a9
George Finch <P2654772@my365.dmu.ac.uk>
2025-01-13 15:46:10 +0000

    buttons now chnage value in order

--- src/index.js
+++ src/index.js
@@ -2,23 +2,38 @@ import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 
-function Square(value){
-    const [selction, setSelection] = useState(value)
+function Square({id, fun}){
+    
     return (
-    <button>X</button>
+    <button id = {id} onClick = {fun}  >" "</button>
 
 )}
 function Board(){
+    const [turn, setTurn] = useState(0)
+    
+    function selected(id){
+        let XOrO = "x"
+        if (turn % 2 == 0){
+            XOrO = "x"
+            setTurn((turn + 1)%2) 
+        } else {
+            XOrO = "o"
+            setTurn((turn + 1)%2) 
+        }
+        
+        document.getElementById(id).textContent = XOrO;
+    console.log("hit")
+    } 
 return(
     <>
     <div>
-    <Square/> <Square/> <Square/>
+    <Square id = "Square1" fun={() => selected("Square1")}/> <Square id = "Square2" fun={() => selected("Square2")}/> <Square id = "Square3" fun={() => selected("Square3")}/>
     </div>
     <div>
-    <Square/> <Square/> <Square/>
+    <Square id = "Square4" fun={() => selected("Square4")}/> <Square id = "Square5" fun={() => selected("Square5")}/> <Square id = "Square6" fun={() => selected("Square6")}/>
     </div>
     <div>
-    <Square/> <Square/> <Square/>
+    <Square id = "Square7" fun={() => selected("Square7")}/> <Square id = "Square8" fun={() => selected("Square8")}/> <Square id = "Square9" fun={() => selected("Square9")}/>
     </div>
     </>
 
